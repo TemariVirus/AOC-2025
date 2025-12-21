14c28c595ae45e2de63c578bdd921a3903ba461b
TemariVirus <kiyonnxperiaz@gmail.com>
2025-12-22 03:15:38 +0800

    Day 1

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Development shell with C3";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ c3c c3-lsp ]; };
+      });
+}
